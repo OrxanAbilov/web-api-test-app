c8508a71c22e278bd1b2125e6ee62da607a2f3dc
OrxanAbilov <abilov_orxan@hotmail.com>
2022-11-12 08:48:26 +0300

    Get Methods

---
+++ my-books/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using my_books.Data.ViewModel;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BookService _bookService;
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var books = _bookService.GetAllBooks();
+            return Ok(books);
+        }
+
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _bookService.GetBookById(id);
+            return Ok(book);
+        }
+
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody]BookVm book)
+        {
+            _bookService.AddBook(book);
+            return Ok();
+        }
+    }
+}
---
+++ my-books/Data/Services/BookService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using my_books.Data.Models;
+using my_books.Data.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace my_books.Data.Services
+{
+    public class BookService
+    {
+        public AppDbContext _context;
+
+        public BookService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+       public void AddBook(BookVm book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead : null,
+                Rate = book.IsRead ? book.Rate : null,
+                Genre = book.Genre,
+                Author = book.Author,
+                CoverUrl = book.CoverUrl,
+                DateAdded = DateTime.Now
+
+            };
+            _context.Add(_book);
+            _context.SaveChanges();
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            List<Book> books = _context.Books.ToList();
+            return books;
+        }
+
+        public Book GetBookById(int id)
+        {
+            Book book = _context.Books.Where(n => n.Id == id).FirstOrDefault();
+            return book;
+        }
+
+       
+    }
+}
